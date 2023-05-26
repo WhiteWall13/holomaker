c519955ac157239771ffa134f0ddd7d8ede6bda0
Nicolas Hameau <72542353+WhiteWall13@users.noreply.github.com>
2023-05-26 10:04:09 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
-import ComputersCanvas from "./ComputersCanvas";
-import { ComputerModel } from "./ComputerModel";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ComputersCanvas>
-        <Suspense fallback={null}>
-          <ComputerModel />
-        </Suspense>
-      </ComputersCanvas>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
